37731225e39d6113577b48ecdcade1c4582485e9
nuri arslan <militanzz@hotmail.com>
2021-03-31 21:29:56 +0300

    31/03/2021

---
+++ Dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> dictionarys = new Dictionary<int, string>();
+            dictionarys.Add(36,"Ahmet");
+            dictionarys.Add(21,"Mehmet");
+            dictionarys.Add(30,"Nuri");
+            dictionarys.Add(34,"Murat");
+            dictionarys.Add(28,"Funda");
+
+            foreach (var dictionary in dictionarys)
+            {
+                Console.WriteLine("Yaşı " + dictionary.Key + " Adı = " + dictionary.Value);
+            }
+        }
+
+
+    }
+}
