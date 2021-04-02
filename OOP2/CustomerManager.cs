0e8513b212407996213eec83034328ad320b8e8d
nuri arslan <militanzz@hotmail.com>
2021-04-02 16:41:04 +0300

    02/04/2021

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //İş sınıflar - Business Classes
+    class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+            //if (musteri)
+            //{
+
+            //}
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileBaseLoggerService = new FileBaseLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { new SmsLoggerService(), new FileBaseLoggerService()};
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(),loggers);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasıtKrediManager};
+            //basvuruManager.KrediOnBİlgilendirmesiYap(krediler);
+            
+        }
+    }
+}
