e4135f07564d19bb17ab5fd4697bc0bc292affa8
nuri arslan <militanzz@hotmail.com>
2021-04-03 15:31:20 +0300

    03/04/2021

---
+++ MultiplyOf3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplyOf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                if (i%3==0)
+                
+                    Console.WriteLine("*");
+                
+                else
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
